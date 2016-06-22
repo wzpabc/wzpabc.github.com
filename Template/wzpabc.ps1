cad067716869fda315eecc74ecffc7f675cf2114
wzpabc <wzpabc@outlook.com>
2016-06-22 17:29:29 +0800

    s

--- Template/wzpabc.ps1
+++ Template/wzpabc.ps1
@@ -27,15 +27,40 @@
         ,'1:6 神说：“诸水之间要有空气，将水分为上下。”                                          '
         ,'</p>                                                               '
         ,'<p>                                                                '
-        ,'	                                                                 '
+        ,'1:1 起初 神创造天地。<br/>                                                 '
+        ,'1:2 地是空虚混沌，渊面黑暗； 神的灵运行在水面上。<br/>                                   '
+        ,'1:3 神说：“要有光。”就有了光。<br/>                                            '
+        ,'1:4 神看光是好的，就把光暗分开了。<br/>                                           '
+        ,'1:5 神称光为昼，称暗为夜。有晚上，有早晨，这是头一日。<br/>                                 '
+        ,'1:6 神说：“诸水之间要有空气，将水分为上下。”                                          '
         ,'</p>                                                               '
-        ,'<h2>Skill and hobit</h2>                                           '
-        ,'<p>Both working and lift are important:</p>                        '
-        ,'<ul>                                                               '
-        , $category   
-        ,'                                                                   '
-        ,'</ul>                                                              '
-        )            |Out-File 'home.html' -Encoding utf8                                                     
+        ,'<p>                                                                '
+        ,'1:1 起初 神创造天地。<br/>                                                 '
+        ,'1:2 地是空虚混沌，渊面黑暗； 神的灵运行在水面上。<br/>                                   '
+        ,'1:3 神说：“要有光。”就有了光。<br/>                                            '
+        ,'1:4 神看光是好的，就把光暗分开了。<br/>                                           '
+        ,'1:5 神称光为昼，称暗为夜。有晚上，有早晨，这是头一日。<br/>                                 '
+        ,'1:6 神说：“诸水之间要有空气，将水分为上下。”                                          '
+        ,'</p>                                                               '
+        #,'<p>                                                                '
+        #,'	                                                                 '
+        #,'</p>                                                               '
+        #,'<h2>Skill and hobit</h2>                                           '
+        #,'<p>Both working and lift are important:</p>                        '
+        #,'<ul>                                                               '
+        #, $category   
+        #,'                                                                   '
+        #,'</ul>                                                              '
+        )            |Out-File 'home.html' -Encoding utf8         
+        
+          @(
+             '<h2>Archive</h2>                                           '
+            ,'<p>Both working and lift are important:</p>                        '
+            ,'<ul>                                                               '
+            , $category   
+            ,'                                                                   '
+            ,'</ul>                                                              '
+        )               |Out-File 'sidebartop.html' -Encoding utf8                              
     }
      
     
