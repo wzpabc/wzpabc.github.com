618806076a9aade005318c2a65554ed67497b6bc
wzpabc <wzpabc@outlook.com>
2016-06-27 14:31:50 +0800

    F

---
+++ Template/Untitled1.ps1
@@ -0,0 +1,235 @@
+﻿cls
+cd E:\ISOFTSTONE\EBook\DBA\数据库笔记
+ls *.sql|%{$_.fullname}
+
+cat ''|co
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\16进制转10进制.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\2005转2000.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\2008转到2000.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\2008转到2005.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\BCP DYNAMIC.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\BCP.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\bcp1.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\BCPALL.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\BOM1.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\BOM2.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\BOM循环.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\BOM按节点排序应用实例.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\BOM查找父结点和子结.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\BOM深度广度排序.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\BOM结构.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\Bulk Insert (2).sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\Bulk Insert.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\CTE实现递归.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\EXEC和sp_executesql的区别.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\IO.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\JOIN的用法.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\MS SQL SERVER数据库置疑后恢复步骤.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\MSSQL中如何用SQL清除所有表的数据.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\MSSQL求連續ID內數量合計-常見解決方案.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\MSSQL計算表達式的值常見解決方案.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\PerfTuningWithSQLServerDMVs_DavidsonFord_scripts.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\sp_MSforeachtable和sp_MSforeachdb用法.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\sp_sql.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\SQL 2000和2005分页存储过程.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\SQL 2005,2008删除过期备份.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\SQL Select语句完整的执行顺序.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\SQL Server 2000连接中的四个最常见错误.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\SQL SERVER 2005 同步复制技术.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\SQL SERVER 数据库实用SQL语句.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\sql server2005安全管理之用户、角色、架构 与 权限 .sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\SQL SERVER数据库置疑后恢复步骤.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\Sql server表值函数.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\SQL Server连接中的四个最常见错误.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\sql xml 入门.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\SQL 多列取最大或者最小值.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\SQL 性能调优综述.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\sql 查询慢的48个原因分析.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\SQL2000同步复制.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\SQL2005下利用XML进行项目的合并与拆分.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\SQL2005更改排序规则.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\SQL2005的安装问题汇总.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\SQL2008 Merge关键字用法.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\SQL2008 Merge关键字用法与简例.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\sql之left join、right join、inner join的区别.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\SQL事务日志还原.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\SQL优先级.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\SQL优化.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\SQL备份与还原经典文档.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\SQL备份和还原.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\SQL复制表结构的通用存储过程.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\SQL数据库加密.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\SQL断电的处理方法.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\SQL求中位数.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\SQL经典算法.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\SQL转EXCEL.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\SQL邮件操作.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\SQL防注入.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\Tempdb.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\Transact-SQL语句导入导出.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\XML在T－SQL查询中的典型应用.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\不同服务器不同服务器数据库之间的数据操作.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\不同服务器数据库之间的数据操作.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\事务、阻塞、统计信息.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\作业.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\使用Log Explorer查看和恢复数据.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\使用PARSENAME拆分字符串.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\使用聚集索引和非聚集索引的区别.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\全文索引.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\全文索引1.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\全角半角转换.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\减少TEMP库.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\分区表示例.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\分区视图和树型的实现.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\分段截取函数.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\分解字符串包含的信息值后然后合并到另外一表的信息.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\删除数据库中所有外键约束.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\删除重复记录.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\利用触发器实现标识列连续.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\动态sql语句基本语法.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\动态sql语句基本语法1.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\动态sql语句用法.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\动态查询.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\化解字符串不能超过8000的方法.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\去汉字.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\取n到m行的值.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\只有mdf文件的恢复技术.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\各种字符串分函数.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\合并列值.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\合并拆分表.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\同步两个数据库的示例.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\处理表重复记录(查询和删除).sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\处理表重复记录.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\备份作业.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\复制表结构的通用存储过程.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\如何写出性能优良的SQL.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\如何删除 SQL2005 过期的数据库备份文件.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\如何利用日志还原.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\如何向一个自增字段插值？.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\如何用SQL语句在两个数据库间复制存储过程.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\如何给MS SQL数据库减肥.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\字母排序.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\字符串分拆.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\字符串的拆分 (2).sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\字符串的拆分.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\存储过程调用存储过程.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\导出表视图中的数据为html 文件.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\将一列中所有的值一行显示.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\将表数据旋转90度.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\将表数据生成SQL脚本的存储过程.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\将表查询存储为标准的XML文件.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\小写转大写.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\小写转大写金额.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\小小总结.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\小计合计的用法.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\已知表名查数据库名.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\库存先进先出.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\异机备份.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\循环数据库压缩日志文件.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\批量删除多个表的多个字段.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\批量删除表.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\找出在2个日期之间的日期.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\拆分、合并、切换分区.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\拼音处理.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\排名.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\排序问题.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\提取中文、英文、汉字.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\搜索指定数据在那个对象中存在.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\数字转换成EXCEL列名.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\数据库减肥.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\数据库学习笔记.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\数据库笔记2.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\数据库笔记3.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\数据库笔记4.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\数据汇总.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\数据结构比较和表结构比较.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\断号查询.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\新建 文本文档 (2).sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\新建 文本文档 (3).sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\新建 文本文档 (4).sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\新建 文本文档 (5).sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\新建 文本文档 (6).sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\新建 文本文档 (7).sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\新建 文本文档 (8).sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\新建 文本文档.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\日志处理.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\日期.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\日期函数.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\日期算法2.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\时间格式转换问题 .sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\显示索引消耗的磁盘情况.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\服务器登录的一些脚本.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\构造使用IN子句的动态Transact-SQL方法.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\构造使用IN子句的动态Transact-SQL方法进行编号查询.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\查找丢失的索引.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\查找指定節點下的子結點和父节点.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\查找无用的索引.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\查每个分组前N条记录.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\查看存储过程依赖的表.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\查看指定spid的脚本当前运行情况和状态.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\查看锁信息.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\查询各节点的父路径函数.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\查询指定节点及其所有子节点.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\标识列的处理.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\检查给定串是否存在于由区间及点集的给合内.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\求排名.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\流水号自增.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\深度排序.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\清理过大的日志文件.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\生成三个SQL随机数的方法.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\生成流水号.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\笔记一.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\笔记七.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\笔记三.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\笔记二.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\笔记五.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\笔记六.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\笔记四.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\系统数据库中查询表的所有字段以及描述（2008，2000区分）以及查询表的外键.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\系统表说明.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\索引DMV.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\索引碎片.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\经典代码.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\给TEMP减肥.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\自动备份数据库.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\自动生成存储过程格式.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\自动生成测试代码(小楼）.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\自动生成测试代码(石头）.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\自增列的处理.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\获取所有的存储过程脚本.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\获取拼音字母.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\获取汉字首字母.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\行列互换.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\行转列.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\表信息查询.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\表结构信息查询.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\计算年龄.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\读写逻辑读执行计划.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\通过脚本创建JOB.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\配置SQL内存.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\重建重组索引的方法.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\锁概念以及例程说明.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\阳历转阴历.sql
+E:\ISOFTSTONE\EBook\DBA\数据库笔记\随机插入汉字.sql
+
+cat E:\ISOFTSTONE\EBook\DBA\数据库笔记\复制表结构的通用存储过程.sql
+cat E:\ISOFTSTONE\EBook\DBA\数据库笔记\随机插入汉字.sql|ConvertTo-Html -
+function tohtml{
+Param(
+[Parameter(ValueFromPipeline=$true)]
+    $txt
+)
+begin{$str='<p>'}
+Process
+{
+     FOREACH ($R IN $txt){
+        $str=$str+"$R<br />`n"
+     }
+}
+end{$str=$str+'</p>';$str}
+}
+
+cat E:\ISOFTSTONE\EBook\DBA\数据库笔记\随机插入汉字.sql|tohtml
+$null| convertto-html -title "windows services: server01" -body (cat E:\ISOFTSTONE\EBook\DBA\数据库笔记\随机插入汉字.sql|tohtml) -Head '<meta http-equiv="Content-Type" content="text/html; charset=utf-8">' -pre "<p>generated by corporate it</p>" -post "for details, contact corporate it."
+$null| convertto-html -title "windows services: server01" -body (cat E:\ISOFTSTONE\EBook\DBA\数据库笔记\读写逻辑读执行计划.sql|tohtml)-Head '<meta http-equiv="Content-Type" content="text/html; charset=utf-8">'  -post "uploaded by Patrick. $(Get-Date)">services.htm;  
+
---
+++ Template/Untitled2.ps1
@@ -0,0 +1,87 @@
+﻿
+function insert-doc
+{
+    param(
+        [Parameter(Position=2)]
+        [ValidateSet('Mysql','Basketball','SQL SERVER','RLang','Java','SSPS','Oracle','Hadoop','Linux(Centos)','BigData')]
+        [System.String]$category,
+        [Parameter(Position=2)]
+        [ValidateSet( 'Home','Java','BigData','Rlang')]
+        [System.String]$menu,
+        [Parameter(Position=0)]
+        [System.String]$source,
+        [Parameter(Position=1)]
+        [System.String]$pagename
+
+    )
+
+
+    begin{
+    #category
+    #030301E0-1B5F-4370-9B80-1E8F582568E5	Mysql
+    #94DB23A4-F7F5-4C4D-9FA4-30B9283C35B6	Basketball
+    #360936B7-A092-426A-8C70-35CC375709F8	SQL SERVER
+    #7DF79285-F2BF-462A-B1DF-504BB1B4F101	RLang
+    #D25C570B-A5A0-44DC-81F1-7327F5EFBF55	Java
+    #F06CD0D2-1CA3-4CCC-B6BB-B81CC83D8702	SSPS
+    #3BB75C2D-062D-4277-9905-B9DE55460C71	Oracle
+    #E12B767F-D8DD-40CF-8CCA-CA1DC9A79FB7	Hadoop
+    #33B6D64C-7147-41D6-A755-EE1B1CA15B3A	Linux(Centos)
+    #menu
+    #0E1793C6-9F68-40C6-9ED8-3853FB3E6190	Home
+    #07815809-720D-4D3C-B609-3FFD563628F7	Java
+    #8F76841D-7738-4DE3-B4DF-70F993BF2160	BigData
+    #FE42E14A-47E3-43F9-A0FF-E14A979B5899	Rlang
+    #5F0006FB-E19B-4E17-8E87-EE612467AFE8	txt    
+    $menuid=''
+    $categoryid=''    
+    }
+    process{
+        if($category -eq 'Mysql'){$categoryid='030301E0-1B5F-4370-9B80-1E8F582568E5'}
+        elseif($category -eq 'Basketball'){$categoryid='94DB23A4-F7F5-4C4D-9FA4-30B9283C35B6'}
+        elseif($category -eq 'SQL SERVER'){$categoryid='360936B7-A092-426A-8C70-35CC375709F8'}
+        elseif($category -eq 'RLang'){$categoryid='7DF79285-F2BF-462A-B1DF-504BB1B4F101'}
+        elseif($category -eq 'Java'){$categoryid='D25C570B-A5A0-44DC-81F1-7327F5EFBF55'}
+        elseif($category -eq 'SSPS'){$categoryid='F06CD0D2-1CA3-4CCC-B6BB-B81CC83D8702'}
+        elseif($category -eq 'Oracle'){$categoryid='3BB75C2D-062D-4277-9905-B9DE55460C71'}
+        elseif($category -eq 'Hadoop'){$categoryid='E12B767F-D8DD-40CF-8CCA-CA1DC9A79FB7'}
+        elseif($category -eq 'Linux(Centos)'){$categoryid='33B6D64C-7147-41D6-A755-EE1B1CA15B3A'}
+        else{$categoryid='D25C570B-A5A0-44DC-81F1-7327F5EFBF55'}
+
+        if($menu -eq 'Home'){
+             $menuid='0E1793C6-9F68-40C6-9ED8-3853FB3E6190'
+        }
+        elseif($menu -eq 'Java'){
+             $menuid='07815809-720D-4D3C-B609-3FFD563628F7'
+        }
+        elseif($menu -eq 'BigData'){
+             $menuid='8F76841D-7738-4DE3-B4DF-70F993BF2160'
+        }
+        elseif($menu -eq 'Rlang'){
+             $menuid='FE42E14A-47E3-43F9-A0FF-E14A979B5899'
+        } 
+        else{  $menuid='07815809-720D-4D3C-B609-3FFD563628F7'} 
+
+    }
+    end{
+        $sql="INSERT INTO pages (id,pagename,html,createdate,[desc],category,mid,source)VALUES(newid(),N'$pagename','',getdate(),'','$categoryid','$menuid',N'$source')"
+        $sql
+        Invoke-Sqlcmd -ServerInstance . -Database wzpabc  -Query $sql
+    }
+}   
+
+
+
+
+cd e:\temp
+cls
+ls *.sql -Recurse |%{insert-doc -category $_.Directory.Name -pagename $_.BaseName -source $_.FullName -menu BigData}
+
+
+
+
+
+
+
+
+ 
